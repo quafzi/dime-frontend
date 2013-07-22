ee1c5e7679ac98f3503a0c5019019e7099d75799
Thomas Birke <tbirke@netextreme.de>
2013-07-22 20:03:10 +0200

    add tracking app

--- apps.js
+++ apps.js
@@ -12,6 +12,9 @@ module.exports =  {
       stylus: '',
       publicConfig: ''
     },
+    tracking: {
+      base: ''
+    },
     angular: {
       lib: '',
     },
---
+++ apps/tracking/base/index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  server: [
+    'gleeman:config:base',
+    'gleeman:express:express',
+    'gleeman:express:server',
+    'page:defaultRenderArgs:get',
+    function(done, config, express, expressServer, renderArgs) {
+      server = express();
+
+      server.set('views', __dirname + '/views');
+      server.set('view engine', 'jade');
+      server.get('/', function(req, res) {
+        var args = renderArgs(req);
+        req.lastname = args.lastname;
+        res.render('index', args);
+      });
+      expressServer.use(server);
+      done(null, server);
+  }, 'page:base:server'],
+  // add public js
+  scripts: [
+    'gleeman:js:file',
+    'gleeman:express:server',
+    function(done, add, server) {
+      add(__dirname + '/public/*.js', 100);
+      done();
+    },
+    'page:base:scriptComplete'
+  ],
+  // add styles
+  stylus: [
+    'page:stylus:add',
+    function(done, add) {
+      add(__dirname, 'style/*.styl');
+      done();
+    },
+    'page:base:styleComplete'
+  ]
+}
