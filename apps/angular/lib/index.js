2c502284905519154f455ff3584cc9b6ce491c6f
Thomas Birke <tbirke@netextreme.de>
2013-07-22 20:03:27 +0200

    include German locale

--- apps/angular/lib/index.js
+++ apps/angular/lib/index.js
@@ -7,6 +7,7 @@ module.exports = {
       console.log('adding angular');
       addJs(__dirname + '/public/angular.js', 1000);
       addJs(__dirname + '/public/angular-webstorage.js', 900);
+      addJs(__dirname + '/public/angular-locale_de-de.js', 900);
       done();
     },
     'page:base:scriptComplete'
