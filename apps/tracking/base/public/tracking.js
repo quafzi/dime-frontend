c0c1ca772af2562eddbf137f99c5e89f0112e581
Thomas Birke <quafzi@netextreme.de>
2013-07-23 23:10:13 +0200

    apply activity filters

--- apps/tracking/base/public/tracking.js
+++ apps/tracking/base/public/tracking.js
@@ -2,6 +2,47 @@
 
 function TrackingCtrl($scope, $http) {
   loadData($scope, $http);
+
+  $scope.applyFilter = function(activity) {
+    console.log($scope.filter);
+    if ('undefined' == typeof($scope.filter)) {
+      return true;
+    }
+
+    if ('object' === typeof($scope.filter.customer)
+      && null !== $scope.filter.customer
+    ) {
+      if (activity.customer.id !== $scope.filter.customer.id) {
+        return false;
+      }
+    }
+
+    if ('object' === typeof($scope.filter.project)
+      && null !== $scope.filter.project
+    ) {
+      if (activity.project.id !== $scope.filter.project.id) {
+        return false;
+      }
+    }
+
+    if ('object' === typeof($scope.filter.service)
+      && null !== $scope.filter.service
+    ) {
+      if (activity.service.id !== $scope.filter.service.id) {
+        return false;
+      }
+    }
+
+    if ('object' === typeof($scope.filter.tag)
+      && null !== $scope.filter.tag
+    ) {
+      if (activity.tag.id !== $scope.filter.tag.id) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 FilterCtrl.$inject = ['$scope', '$http'];
 
